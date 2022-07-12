a2e456f961b0923c9dc939d4ad7887c98542d5b2
Kurin Pavel <kpa306s@gmail.com>
2022-07-12 20:38:47 +0400

    Add Nullable property

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -7,23 +7,18 @@ class WallService {
         val changeId = if (posts.size != 0) posts.last().id + 1 else 1
         val postNext = post.copy(id = changeId)
         posts += postNext
-
         return posts.last()
     }
 
     fun update(post: Post): Boolean {
         var change = false
         for ((index, value) in posts.withIndex()) {
-            if (value.id != post.id) {
-                change = false
-                continue
-            } else {
+            if (value.id == post.id) {
                 posts[index] = value.copy(
                     ownerId = value.ownerId,
                     date = value.date
                 )
                 change = true
-                break
             }
         }
         return change
